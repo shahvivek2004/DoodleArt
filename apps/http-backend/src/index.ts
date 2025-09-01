502f7c474a384fdef6dc69708ebfcef57410f5f8
Tyler Durden <techiguy2004@gmail.com>
2025-09-01 18:23:28 +0530

    Enabled prod config in HTTP

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -21,9 +21,9 @@ export const FE_URL = process.env.FE_URL;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const EXP_TIME = process.env.EXP_TIME;
 // prod
-//const cookieConfig = { domain: '.doodleart.live', httpOnly: true, secure: true, sameSite: 'lax' as const, maxAge: (1000 * 60 * 60 * 24 * 4) };
+const cookieConfig = { domain: '.doodleart.live', httpOnly: true, secure: true, sameSite: 'lax' as const, maxAge: (1000 * 60 * 60 * 24 * 4) };
 //dev
-const cookieConfig = { httpOnly: true, secure: true, sameSite: 'lax' as const, maxAge: (1000 * 60 * 60 * 24 * 4) };
+//const cookieConfig = { httpOnly: true, secure: true, sameSite: 'lax' as const, maxAge: (1000 * 60 * 60 * 24 * 4) };
 const app = express();
 
 app.use(helmet());
